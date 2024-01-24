328027e68e0483e1b3e27a902328fd905ca46e23
Ahmet_Toy <ahmet.toy@tu-darmstadt.de>
2024-01-24 23:18:31 +0100

    feat: export messages to log-file

--- src/utils/log/Logger.java
+++ src/utils/log/Logger.java
@@ -1,14 +1,19 @@
 package utils.log;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class Logger {
 
   private static final List<String> logMessages = new ArrayList<>();
+  private static Writer writer;
 
   public static void log(String message) {
-    logMessages.add(message);
+    logMessages.add("- " + message);
+    exportToLogFile();
   }
 
   public static String[] getLog() {
@@ -19,7 +24,13 @@ public final class Logger {
     return logArray;
   }
 
-  public static void exportToLogFile() {
-    // TODO (Ahmet): implement
+  private static void exportToLogFile() {
+    try {
+      writer = new FileWriter("C:/Users/toy/IdeaProjects/HeliLocationOptimizer/src/utils/log/log.log");
+      for (String message : logMessages) {
+        writer.write(message + System.lineSeparator());
+      }
+      writer.close();
+    } catch (IOException ignored) {}
   }
 }
