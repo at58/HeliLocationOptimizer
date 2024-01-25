7acc637c39dec518b9cf48a869e7953201da4f3a
Ahmet_Toy <ahmet.toy@tu-darmstadt.de>
2024-01-26 00:56:05 +0100

    chore: extended gui

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,6 +1,8 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,23 +12,32 @@ import javax.swing.JTabbedPane;
 /**
  * Test GUI
  */
-public class GUI extends JFrame {
+public final class GUI extends JFrame {
+
+  private final Font consolas14 = new Font("consolas", Font.BOLD, 14);
+  private final Font consolas16 = new Font("consolas", Font.BOLD, 16);
 
   private final Toolkit tk = Toolkit.getDefaultToolkit();
 
   public GUI() {
-    setTitle("GUI");
+    setTitle("Helicopter Location Optimizer");
     Dimension dimension = tk.getScreenSize();
-    setSize((int) dimension.getWidth(), (int) dimension.getHeight() - 50);
+    int widht = (int) dimension.getWidth();
+    int height = (int) dimension.getHeight() - 50;
+    setSize(widht, height);
+    setLocation((int) (dimension.getWidth() - widht)/2,5);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     JTabbedPane tabbedPane = new JTabbedPane();
+    tabbedPane.setBackground(Color.ORANGE);
+    tabbedPane.setFont(consolas16);
 
     JPanel tab1 = new JPanel();
-    tab1.add(new JLabel("Inhalt von Tab 1"));
+    tab1.add(new JLabel("Elemente von Tab 1..."));
     tabbedPane.addTab("Karte", tab1);
 
     JPanel tab2 = new JPanel();
-    tab2.add(new JLabel("Inhalt von Tab 2"));
+    tab2.add(new JLabel("Elemente von Tab 2..."));
     tabbedPane.addTab("Daten", tab2);
 
     add(tabbedPane);
