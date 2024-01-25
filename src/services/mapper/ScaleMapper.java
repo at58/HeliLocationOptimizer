205c6a1e8404f2bcc8d99a9e46554e0ea474cd76
Ahmet_Toy <ahmet.toy@tu-darmstadt.de>
2024-01-26 00:56:39 +0100

    feat: determine axis scales

--- src/services/mapper/ScaleMapper.java
+++ src/services/mapper/ScaleMapper.java
@@ -9,24 +9,45 @@ import java.util.Map;
 
 public class ScaleMapper {
 
-  public static Object specifyAxisScales(List<Location> locationList,
+  /**
+   * scales the range of the locations to the axes of the map.
+   *
+   * @param locationList the list of all locations.
+   * @param screen the size of the screen.
+   * @param partitions the number of axis separations.
+   * @return Map of String-key consisting of the axis with the conversion key and the array of intervals
+   */
+  public static Map<String, int[]> specifyAxisScales(List<Location> locationList,
                                          Dimension screen,
                                          int partitions) {
 
     Map<String, int[]> axisScales;
     if (partitions < 2) {
-      // exception
+      throw new IllegalArgumentException("Die Anzahl der Partitionen ist kleiner als 2");
     } else {
       axisScales = new HashMap<>();
       int[][] ranges = determineAxisRanges(locationList);
       int x_range = ranges[0][1] - ranges[0][0];
       int y_range = ranges[1][1] - ranges[1][0];
-      int xInterval = x_range / partitions;
-      int yInterval = y_range / partitions;
+      int x_axis = (int) screen.getWidth() - 100;
+      int y_axis = (int) screen.getHeight() - 100;
+      double x_conversionKey = (double) x_range / x_axis;
+      double y_conversionKey = (double) y_range / y_axis;
+      int x_interval = (int) ((x_range / partitions) * x_conversionKey);
+      int y_interval = (int) ((y_range / partitions) * y_conversionKey);
 
+      int[] x_scale = new int[partitions + 1];
+      int[] y_scale = new int[partitions + 1];
+
+      for (int i = 1; i <= partitions ; i++) {
+        x_scale[i] = i * x_interval;
+        y_scale[i] = i * y_interval;
+      }
+      axisScales.put("x-Axis/1:" + x_conversionKey, x_scale);
+      axisScales.put("y-Axis/1:" + y_conversionKey, y_scale);
     }
 
-    return null;
+    return axisScales;
   }
 
   public static int[][] determineAxisRanges(List<Location> locationList) {
--- test/test/ScaleMapperTest.java
+++ test/test/ScaleMapperTest.java
@@ -1,7 +1,11 @@
 package test;
 
 import core.Location;
+import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import services.mapper.ScaleMapper;
 
@@ -10,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ScaleMapperTest {
 
   @Test
-  void scaleTest() {
+  void axisRangeTest() {
     // arrange
     List<Location> locationList = List.of(
         new Location("StadtA", 20, 50, 22),
@@ -23,5 +27,27 @@ public class ScaleMapperTest {
     int[][] actual = ScaleMapper.determineAxisRanges(locationList);
     // assert
     assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  void axisScaleTest() {
+    // arrange
+    List<Location> locationList = List.of(
+        new Location("StadtA", 20, 50, 22),
+        new Location("StadtB", 5, 29, 22),
+        new Location("StadtC", 66, 12, 22),
+        new Location("StadtD", 120, 65, 22)
+    );
+    Map<String, int[]> expected = Map.of("x-Axis/1:",
+                                         new int[] {10, 20, 30, 40, 50, 60, 70, 80, 90, 100},
+                                         "y-Axis/",
+                                         new int[] {5, 10, 15, 20, 25, 30, 35, 40, 45, 50});
+    // act
+    Map<String, int[]> actual = ScaleMapper.specifyAxisScales(locationList,
+                                                              Toolkit.getDefaultToolkit()
+                                                                     .getScreenSize(), 10);
+    List<String> keyList = new ArrayList<>(actual.keySet());
+    // assert
+    assertArrayEquals(expected.get("x-Axis/1:"), actual.get(keyList.get(0)));
   }
 }
