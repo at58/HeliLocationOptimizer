2571749cb39cfa51768b8a17def737bbdf5244c6
Ahmet_Toy <ahmet.toy@tu-darmstadt.de>
2024-01-26 22:59:31 +0100

    feat: csv template exporter

---
+++ src/services/exporter/CsvTemplateExporter.java
@@ -0,0 +1,31 @@
+package services.exporter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import services.saving.StorageService;
+import utils.log.Logger;
+
+public class CsvTemplateExporter {
+
+  public static String export() {
+
+    File storageTarget = StorageService.getStorageLocation("Ski_Gebiet_Daten.csv");
+
+    try {
+      FileWriter writer = new FileWriter(storageTarget);
+      List<String> header = List.of("Ort", "x-Koordinate", "y-Koordinate", "Unfallzahl p.a.");
+      writeLine(writer, header);
+      writer.close();
+    } catch (IOException e) {
+      Logger.log(e.getMessage());
+    }
+    return storageTarget.getAbsolutePath();
+  }
+
+  private static void writeLine(FileWriter writer, List<String> data) throws IOException {
+    String csvLine = String.join(";", data) + System.lineSeparator();
+    writer.write(csvLine);
+  }
+}
