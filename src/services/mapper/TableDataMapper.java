7130232a135ce729fb83e5c5437987918ec860da
Ahmet_Toy <ahmet.toy@tu-darmstadt.de>
2024-02-05 15:36:14 +0100

    feat: mapping from table to String arrays

--- src/services/mapper/TableDataMapper.java
+++ src/services/mapper/TableDataMapper.java
@@ -1,7 +1,9 @@
 package services.mapper;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
 
 public class TableDataMapper {
 
@@ -11,13 +13,26 @@ public class TableDataMapper {
     for (int i = 1; i < csvTuples.size(); i++) {
       Object[] modelTuple = new Object[4];
       String[] csvTuple = csvTuples.get(i);
-      Arrays.stream(csvTuple).forEach(System.out::println);
       modelTuple[0] = csvTuple[0];
       modelTuple[1] = Integer.parseInt(csvTuple[1]);
       modelTuple[2] = Integer.parseInt(csvTuple[2]);
       modelTuple[3] = Integer.parseInt(csvTuple[3]);
       result[i-1] = modelTuple;
     }
+    return result;
+  }
+
+  public static List<String[]> mapToStringArrayList(Vector<Vector> dataVector) {
+
+    List<String[]> result = new ArrayList<>();
+    dataVector.forEach(vector -> {
+      Object[] objects = vector.toArray();
+      String[] strings = new String[objects.length];
+      for (int i = 0; i < objects.length; i++) {
+        strings[i] = String.valueOf(objects[i]);
+      }
+      result.add(strings);
+    });
     return result;
   }
 }
