cf993eb297efc300b90a9762c55767e67d9e0cc9
Ahmet_Toy <ahmet.toy@tu-darmstadt.de>
2024-02-10 16:32:54 +0100

    refactor: seperated LocationFinder methods

--- src/services/calculations/LocationFinder.java
+++ src/services/calculations/LocationFinder.java
@@ -1,11 +1,16 @@
 package services.calculations;
 
+import domain.Coordinate;
 import domain.Helicopter;
 import domain.Location;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import services.mapper.LocationMapper;
+import services.mapper.ScaleMapper;
 import utils.exceptions.NoLocationDataException;
 
 public class LocationFinder {
@@ -33,4 +38,46 @@ public class LocationFinder {
 
 
   }
+
+  public static void determinePreDistribution(List<Location> locationList, int helicopterNumber) {
+
+    List<List<Location>> sectoredLocations = generateAndAssignToSectors(locationList, helicopterNumber);
+
+  }
+
+  public static List<List<Location>> generateAndAssignToSectors(
+      List<Location> locationList,
+      int helicopterNumber) {
+
+    List<List<Location>> distributionSectors = new ArrayList<>();
+    for (int i = 0; i < helicopterNumber; i++) {
+      distributionSectors.add(new ArrayList<Location>());
+    }
+    int[][] dimension = ScaleMapper.determineAxisDimensions(locationList);
+    int[] xPosition = new int[2];
+    xPosition[0] = dimension[0][0]; // min
+    xPosition[1] = dimension[0][1]; // max
+    int range = xPosition[1] - xPosition[0];
+
+    int sectorRange = range / helicopterNumber;
+    int sectorA = xPosition[0]; // left border
+    int sectorB = sectorA + sectorRange; // right border
+    for (int i = 0; i < distributionSectors.size(); i++) {
+      for (Location location : locationList) {
+        Coordinate coordinate = location.getCoordinate();
+        if (coordinate.x() >= sectorA
+            && coordinate.x() < sectorB) {
+          distributionSectors.get(i).add(location);
+        }
+      }
+      sectorA = sectorB;
+      if (i == (distributionSectors.size() - 2)) {
+        sectorB = (xPosition[1] + distributionSectors.size());
+      } else {
+        sectorB = (sectorA + sectorRange);
+      }
+    }
+    return distributionSectors;
+  }
+
 }
