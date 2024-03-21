28203f43500b8ee62d4c44164c6147cc808b63e8
itsChrizZ <christoph.sm@e.mail.de>
2024-03-21 21:59:27 +0100

    updated export of solution

--- src/services/exporter/CsvExporter.java
+++ src/services/exporter/CsvExporter.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
@@ -12,23 +11,27 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.commons.csv.CSVPrinter;
 
 import domain.Helicopter;
 import services.dialog.StorageService;
 import utils.exceptions.DialogCancelledException;
 import utils.log.Logger;
 
+/**
+ * CsvExporter provides functionality to export data to CSV and Excel files.
+ */
 public class CsvExporter {
 
 	private static final List<String> header = List.of("Ort", "x-Koordinate", "y-Koordinate", "Unfallzahl pro Jahr");
 	private static final List<String> solutionHeader1 = List.of("Helikopter", "x-Koordinate", "y-Koordinate",
 			"Gesamtunfälle", "Durchschnittliche Unfälle", "Zugeordnete Orte");
-	private static final List<String> solutionHeader2 = List.of("Helikopter", "Ort", "Entfernung", "Flugzeit");
+	private static final List<String> solutionHeader2 = List.of("Helikopter", "Ort", "Entfernung", "Flugzeit (min)");
 
+	/**
+	 * exports a template CSV file with header.
+	 * 
+	 * @return The absolute path to the exported CSV file.
+	 */
 	public static String exportTemplate() {
 
 		String storageLocation;
@@ -51,6 +54,12 @@ public class CsvExporter {
 		return storageLocation;
 	}
 
+	/**
+	 * saves data to a CSV file.
+	 * 
+	 * @param data The data to be saved.
+	 * @return The absolute path to the saved CSV file.
+	 */
 	public static String saveTable(List<String[]> data) {
 
 		String storagePath = "";
@@ -79,53 +88,70 @@ public class CsvExporter {
 		writer.write(csvLine);
 	}
 
+	/**
+	 * saves solution data to an Excel file.
+	 * 
+	 * @param helicopterList The list of helicopters containing solution data.
+	 */
 	public static void saveSolution(List<Helicopter> helicopterList) {
-		try {
-			File storageTarget = StorageService.getStorageLocation("", 3, "xls");
-			Workbook workbook = new HSSFWorkbook();
 
-            // create first sheet
-			Sheet sheet1 = workbook.createSheet("Sheet1");
-            writeLine(sheet1, solutionHeader1);
+		if (helicopterList != null && helicopterList.size() > 0)
+			try {
+				File storageTarget = StorageService.getStorageLocation("", 3, "xls");
+				Workbook workbook = new HSSFWorkbook();
 
-            // create second sheet
-            Sheet sheet2 = workbook.createSheet("Sheet2");
-            writeLine(sheet2, solutionHeader2);
-            
-//            for (Helicopter heli : helicopterList) {
-//                int index = helicopterList.indexOf(heli);
-//                writeLine(sheet1, heli.getSummarizedData(index));
-//
-//                List<String[]> data = heli.getDetailedData(index);
-//                for (String[] tuple : data) {
-//                    writeLine(sheet2, List.of(tuple));
-//                }
-//            }
-//
-            for(int i = 0; i < solutionHeader1.size(); i++) {
-                sheet1.autoSizeColumn(i);
-            }
-            
-            for(int i = 0; i < solutionHeader2.size(); i++) {
-                sheet2.autoSizeColumn(i);
-            }
-            
-            // save file
-            FileOutputStream fileOut = new FileOutputStream(storageTarget);
-            workbook.write(fileOut);
-            fileOut.close();
-            workbook.close();
-        } catch (DialogCancelledException | IOException e) {
-        	Logger.log(e.getMessage());
-        }
+				// create first sheet
+				Sheet sheet1 = workbook.createSheet("Lösung zusammengefasst");
+				writeLine(sheet1, solutionHeader1);
+
+				// create second sheet
+				Sheet sheet2 = workbook.createSheet("Lösung detailliert");
+				writeLine(sheet2, solutionHeader2);
+
+				for (Helicopter heli : helicopterList) {
+					int index = helicopterList.indexOf(heli);
+					writeLine(sheet1, heli.getSummarizedData(index));
+
+					List<String[]> data = heli.getDetailedData(index);
+					for (String[] tuple : data) {
+						writeLine(sheet2, List.of(tuple));
+					}
+				}
+
+				for (int i = 0; i < solutionHeader1.size(); i++) {
+					sheet1.autoSizeColumn(i);
+				}
+
+				for (int i = 0; i < solutionHeader2.size(); i++) {
+					sheet2.autoSizeColumn(i);
+				}
+
+				// save file
+				FileOutputStream fileOut = new FileOutputStream(storageTarget);
+				workbook.write(fileOut);
+				fileOut.close();
+				workbook.close();
+			} catch (DialogCancelledException | IOException e) {
+				Logger.log(e.getMessage());
+			}
 	}
-	
-	private static void writeLine(Sheet sheet, List<String> data) {	
+
+	private static void writeLine(Sheet sheet, List<String> data) {
 		Row row = sheet.createRow(sheet.getPhysicalNumberOfRows());
-		
+
 		for (int i = 0; i < data.size(); i++) {
 			String str = data.get(i);
-			row.createCell(i).setCellValue(str);
+
+			double d = -1;
+			try {
+				d = Double.parseDouble(str);
+				row.createCell(i).setCellValue(d);
+			} catch (NumberFormatException nfe) {
+
+			}
+
+			if (d == -1)
+				row.createCell(i).setCellValue(str);
 		}
 	}
 
